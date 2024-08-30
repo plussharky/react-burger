c4906fad703e60a25c1a5f2af9330a287201a131
Plusharky <plusharky@yandex.ru>
2024-08-30 19:42:20 +0300

    Added API connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,51 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styles from './App.module.css';
 import AppHeader from './components/app-header/app-header';
 import BurgerIngredients from './components/burger-ingredients/burger-ingredients';
 import BurgerConstructor from './components/burger-constructor/burger-constructor';
-import data from './utils/data';
-import dataBurgerComponents from "./utils/data-for-burger-constructor";
+
+const API_URL = "https://norma.nomoreparties.space/api/ingredients"
 
 function App() {
+  const [ingredients, setIngredients] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+      const fetchIngredients = async () => {
+        try {
+          const response = await fetch(API_URL);
+
+          if(!response.ok) {
+            throw new Error(`Сервер вернул ответ не ОК по запросу ${API_URL}`)
+          }
+
+
+          const data = await response.json();
+          setIngredients(data.data)
+        } catch (error) {
+          setError(error)
+        } finally {
+          setLoading(true);
+        }
+    };
+
+    fetchIngredients();
+  }, [])
+
   return (
     <div className="App">
       <AppHeader />
-      <main className={styles.burgerAssembly}>
-        <BurgerIngredients data={data}/>
-        <BurgerConstructor data={dataBurgerComponents}/>
-      </main>
+      {loading ? 
+        (
+          <main className={styles.burgerAssembly}>
+            <BurgerIngredients data={ingredients}/> 
+            <BurgerConstructor data={ingredients}/>
+          </main>
+        )
+      :
+        (<p>Загрузка...</p>)
+      }
     </div>
   );
 }
